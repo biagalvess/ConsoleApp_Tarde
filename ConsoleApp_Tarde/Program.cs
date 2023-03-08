3fcdb2e07a990e8ea50380b9a4f71768ec630b3c
biagalvess <biaalvesmatao@gmail.com>
2023-03-08 15:22:46 -0300

    Criação do metodo Set da classe Produto e instanciei os objetos

--- ConsoleApp_Tarde/Program.cs
+++ ConsoleApp_Tarde/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp_Tarde;
+
 Console.WriteLine("Hello, World!");
+
+Produto p1 = new Produto(1, "Mouse xing ling", 10.90M);
+Produto p2 = new Produto(2,"Teclado multilaser",20.9M);
+
+Console.WriteLine(p1.getDescricao() + " " + p1.getValor());
+Console.WriteLine(p2.getDescricao() + " " + p2.getValor());
+
+p1.setValor(29.90M);
+p1.setDescricao("Mouse Razer");
+
+Console.WriteLine(p1.getValor() + "  " + p1.getValor());
